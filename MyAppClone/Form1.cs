a815a21c91385fa0608810d43e1165b1a4da59c3
Jaume Plomer Suau <jaumeplomer@paucasesnovescifp.cat>
2020-10-09 16:04:26 +0200

    Added a button to the forms wich shows a message when clicked.

--- MyAppClone/Form1.cs
+++ MyAppClone/Form1.cs
@@ -16,5 +16,10 @@ namespace MyAppClone
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Hello There!");
+        }
     }
 }
